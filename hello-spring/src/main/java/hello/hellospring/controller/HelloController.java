ce72972b36bfa0752153d1856bcfa579c63525e6
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-06 16:43:56 +0900

    [feat] view 환경설정 및 helloController, hello.html 작성하여 랜더링

---
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello") // /hello로 들어올 시 해당 method 호출
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!"); // data를 hello!! 라고 넘김.
+        return "hello"; // 랜더링할 화면으로 hello.html 리턴
+    }
+}
