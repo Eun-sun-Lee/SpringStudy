d33d692db47bde2c42ce95538fa5a945e10c4abf
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-06 19:12:45 +0900

    [feat/API] API 방식으로 helloString 함수 만든 후 data(string) 내려주기

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,9 +15,15 @@ public class HelloController {
         return "hello"; // 랜더링할 화면으로 hello.html 리턴
     }
 
-    @GetMapping("hello-mvc")
+    @GetMapping("hello-mvc") // MVC 방식
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string") // API 방식
+    @ResponseBody // http 통신 프로토콜에서 body 부에 return할 data를 직접 넣어줌.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; // 위의 helloMvc와 차이 : view 필요 없이 data(문자)가 그대로 내려감.
     }
 }
