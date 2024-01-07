dd8094ba29098ac5cee8fcfd21f6715941efbb36
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-07 11:15:54 +0900

    [feat/회원관리] 회원 서비스 개발 : join, validateDuplicateMember, findMembers, findOne (비즈니스 로직 관련 함수 개발)

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,41 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // 같은 이름이 있는 중복 회원 검증
+        memberRepository.findByName(member.getName()) // 리턴값 : optional 객체
+                .ifPresent(m -> { // null이 아닌 값이 있다면 (optional 안에 객체 존재)
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    // param으로 온 id에 해당하는 객체 반한
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
