21924adf4bc75658ef06ce205c92f9ca50824428
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-12 16:36:51 +0900

    [feat/회원관리] 회원 웹 기능 - 등록 : /members/new에 get,post mapping 되는 함수 만들어 memberService에 회원가입 시키기

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 스프링 빈 관리 -> 스프링 컨테이너에 memberController 객체 생성하여 넣어둠.
 public class MemberController {
@@ -12,5 +15,20 @@ public class MemberController {
     @Autowired // DI -> memberController 생성시 스프링 빈에 등록된 memberService 객체와 연결시켜줌.
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm){
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; // home 화면으로 redirect
     }
 }
