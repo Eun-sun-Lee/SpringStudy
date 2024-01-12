eeeea699a1408c62964642987b5ab6a3d5782736
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-12 16:52:11 +0900

    [feat/회원관리] 회원 웹 기능 - 조회 : /members에 GetMapping되는 List 함수 만들어 list 형태의 회원들을 model에 넘겨 출력

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 스프링 빈 관리 -> 스프링 컨테이너에 memberController 객체 생성하여 넣어둠.
 public class MemberController {
@@ -28,7 +31,13 @@ public class MemberController {
         member.setName(memberForm.getName());
 
         memberService.join(member);
-
         return "redirect:/"; // home 화면으로 redirect
+    }
+
+    @GetMapping("/members")
+    public String List(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); // 멤버의 리스트 자체를 model에 담아 화면으로 넘김.
+        return "members/memberList";
     }
 }
