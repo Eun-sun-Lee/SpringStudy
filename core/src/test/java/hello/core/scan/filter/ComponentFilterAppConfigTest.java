0249d3f4847c40cb5bb977df04bc061f90db37cf
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-18 02:31:48 +0900

    [feat/컴포넌트 스캔] includeFilters, exculdeFilters 사용하여 컴포넌트 스캔 대상을 추가로 지정 및 제외 / MyIncludeComponent, MyExcludeComponent 어노테이션 만들어서 BeanA, BeanB class에 각각 지정 후 include/excludeFilters 거는 필터 테스트

---
+++ core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,39 @@
+package hello.core.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+
+        assertThat(beanA).isNotNull(); // MyIncludeComponent annotation 적용
+        assertThrows(  // MyExcludeComponent annotation 적용
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class)
+        );
+    }
+
+
+    // 나만의 컴포넌트 스캔할 수 있는 기능이 만들어짐.
+    @Configuration
+    @ComponentScan(
+            includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    )
+    static class ComponentFilterAppConfig{
+
+    }
+}
