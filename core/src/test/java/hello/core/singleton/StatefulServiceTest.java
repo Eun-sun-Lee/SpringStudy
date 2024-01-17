1374b4c16e5445643e2ebfdaa7964ec5c7767878
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-17 20:20:25 +0900

    [feat/싱글톤 컨테이너] 싱글톤 방식의 주의점 : 싱글톤 객체를 무상태로 설계하도록 변경 (공유되는 필드 price의 값을 변경 X하도록 변경 -> 공유되는 전역 필드 price를 삭제

--- core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -16,15 +16,18 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         // Thread A : A 사용자 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         // Thread B : B 사용자 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         // Thread A : A 사용자 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = statefulService1.getPrice();
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        // 지역변수이므로 둘의 값이 다름. (지역변수 -> 값 공유 X)
+        System.out.println("userAPrice = " + userAPrice);
+        System.out.println("userBPrice = " + userBPrice);
+
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
