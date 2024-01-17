48544e566bf4ef1554cc5a227d06a444dbe6fdc5
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-18 01:39:25 +0900

    [feat/컴포넌트 스캔] 탐색 위치와 기본 스캔 대상 : 스프링 부트를 사용하면 @SpringBootApplication 안의 @ComponentScan 때문에 메인 앱의 하위 패키지를 컴포넌트 스캔

--- core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
-
 public class AutoAppConfigTest {
     @Test
     void basicScan(){
