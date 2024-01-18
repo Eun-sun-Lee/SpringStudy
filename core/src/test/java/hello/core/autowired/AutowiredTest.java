f10a85f85659ab8a01828597385a91b7f68eea49
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-19 03:59:35 +0900

    [feat/의존관계 자동 주입] 옵션 처리 : 스프링 빈이 아닌데 @Autowired 사용시 자동 주입 대상을 옵션으로 처리하는 방법 - 1) @Autowired(required=false) 2) @Nullable 3) Optional<>

---
+++ core/src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,38 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import io.micrometer.common.lang.Nullable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);// TestBean이 스프링 빈으로 등록됨.
+    }
+
+    static class TestBean{
+
+        // member는 스프링 빈이 아니므로 null
+        @Autowired(required = false) // 의존관계가 없으므로 메소드 호출 자체가 안됨.
+        public void setNoBean1(Member noBean1){
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2){
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3){
+            System.out.println("noBean3 = " + noBean3);
+        }
+
+    }
+}
