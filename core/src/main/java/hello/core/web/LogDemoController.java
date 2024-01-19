9b8e8459be598ac9434702332c43a284cce2b7e7
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-20 04:03:33 +0900

    [feat/빈 스코프] 스코프와 Provider : ObjectProvider.getObject() 호출 시점까지 request scope 빈의 생성 지연

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger; // request 스코프 -> HTTP request가 들어오고 나가기까지 사용 가능
+    private final ObjectProvider<MyLogger> myLoggerProvider; // request 스코프 -> HTTP request가 들어오고 나가기까지 사용 가능
 
     @RequestMapping("log-demo")
     @ResponseBody // 뷰 화면 없이 문자 반환하고 싶음.
     public String logDemo(HttpServletRequest request) { // 고객 요청 정보를 받을 수 O
-        String requestURL = request.getRequestURL().toString();// 고객이 요청한 URL
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject(); // myLogger 빈 객체 생성
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
