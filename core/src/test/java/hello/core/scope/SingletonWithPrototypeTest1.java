cca467d0139b44aeb27b51aa84bac1eff0fd1b12
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-19 23:23:16 +0900

    [feat/빈 스코프] 프로토타입 스코프 - 싱글톤 빈과 프로토타입 빈을 함께 사용시 싱글톤 빈이 프로토타입을 사용할 때마다 스프링 컨테이너에 새로 요청하여 문제 해결 (But, 좋지 않은 코드)

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -5,6 +5,7 @@ import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -34,7 +35,7 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
         // prototypeBean 새로 생성
 //        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
@@ -44,14 +45,17 @@ public class SingletonWithPrototypeTest1 {
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; // 생성 시점에 prototypeBean 주입
-
+//        private final PrototypeBean prototypeBean; // 생성 시점에 prototypeBean 주입
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        ApplicationContext applicationContext;
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean){
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic(){
+            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
