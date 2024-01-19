c4255e6a9267cf184458f2962952a7bf5e93d1c6
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-19 21:06:23 +0900

    [feat/빈 생명주기 콜백] 애노테이션 @PostConstruct, @PreDestroy : 자바 표준으로 스프링이 다른 컨테이너에서도 잘 동작하므로 권장 / 외부 라이브러리를 초기화, 종료해야 하면 @Bean의 initMethod, destroyMethod 사용

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient { // 초기화 빈, 종료 빈
     private String url; // 접속해야 할 서버의 url
 
@@ -25,12 +28,14 @@ public class NetworkClient { // 초기화 빈, 종료 빈
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init() { // 의존관계 주입이 끝나면 호출
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() { // 스프링이 종료되기 전에 (소멸 전) 콜백
         System.out.println("NetworkClient.close");
         disconnect();
