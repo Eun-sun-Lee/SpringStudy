7043ba58e81bd011880fc785f518cf29db6d71c4
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-26 13:35:24 +0900

    [feat/MVC 프레임워크 만들기] 단순하고 실용적인 컨트롤러 - v4 : v3와 달리 각 컨트롤러에서 ModelView 객체 반환 필요 없이 1) 프론트 컨트롤러로부터 전달받은 model 객체에 값 설정 2) 뷰의 논리 이름을 String type으로 직접 반환 하도록 변경 -> 프레임워크나 공통 기능이 하는 일이 많아짐으로써 컨트롤러를 구현하는 개발자 입장에서 편리해짐.

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -1,14 +1,14 @@
-package hello.servlet.web.frontcontroller.v3;
+package hello.servlet.web.frontcontroller.v4;
 
 import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v2.ControllerV2;
-import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
-import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
-import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -19,16 +19,16 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
-public class FrontControllerServletV3 extends HttpServlet {
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
 
-    private Map<String, ControllerV3> controllerMap = new HashMap<>(); // request url, controller
+    private Map<String, ControllerV4> controllerMap = new HashMap<>(); // request url, controller
 
-    public FrontControllerServletV3() {
+    public FrontControllerServletV4() {
         // Mapping 정보를 담아둠.
-        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
-        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
-        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
     }
 
     @Override
@@ -37,23 +37,23 @@ public class FrontControllerServletV3 extends HttpServlet {
         // 1) requestURI에 매핑된 컨트롤러 객체 생성
         String requestURI = request.getRequestURI();
 
-        ControllerV3 controller = controllerMap.get(requestURI);
+        ControllerV4 controller = controllerMap.get(requestURI);
         if (controller == null){
             response.setStatus(HttpServletResponse.SC_NOT_FOUND); // 404
             return;
         }
         // 2) 각 컨트롤러에서의 불필요한 서블릿 종속을 피하기 위해 key(parameter)-value 값을 저장한 paramMap 생성
         Map<String, String> paramMap = createParamMap(request);
+        Map<String, Object> model = new HashMap<>(); // 추가
 
         // 3) 각 컨트롤러 호출
-        ModelView mv = controller.process(paramMap); // 각 컨트롤러에서는 ModelView 객체만 생성해서 반환함. (ModelView-생성자 호출)
+        String viewName = controller.process(paramMap, model);
 
         // 4) 뷰 리졸버
-        String viewName = mv.getViewName(); // 논리 이름
         MyView myView = viewResolver(viewName); // 물리 이름
 
         // 5) 뷰 렌더링
-        myView.render(mv.getModel(), request, response); // MyView-render 함수 호출
+        myView.render(model, request, response); // MyView-render 함수 호출
     }
 
     private MyView viewResolver(String viewName) {
