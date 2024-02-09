19582eabf70615906cf5bf3e5f1a41992d759e3f
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-02-09 11:56:13 +0900

    [feat/스프링 MVC 기본 기능] 응답 - 정적 리소스, 뷰 템플릿 : 정적 리소스-/static, /public, /resources, /META-INF/resources 등 클래스패스의 다음 디렉토리에 있는 정적 리소스 제공 / 뷰 템플릿-thymeleaf가 default로 prefix에 /templates/, suffix에 .html 붙여서 필요한 스프링 빈 등록 1)ModelAndView에 필요한 모델 속성 추가하여 반환 2) Model을 인자로 받아 모델 속성 지정, 뷰의 논리적 이름 반환 3) 리턴타입이 void이고 Http 메시지 바디를 처리하는 파라미터 인자가 없을시 컨트롤러 경로의 이름과 뷰의 논리적 이름이 같다면 이를 인식하고 반환

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -6,11 +6,13 @@ import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.w3c.dom.html.HTMLBRElement;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -52,5 +54,20 @@ public class RequestBodyJsonController {
     public String requestBodyJsonV1(@RequestBody HelloData helloData) throws IOException{
         log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v4")
+    public String requestBodyJsonV4(HttpEntity<HelloData> httpEntity) {
+        HelloData data = httpEntity.getBody();
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v5")
+    public HelloData requestBodyJsonV4(@RequestBody HelloData data) {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return data;
     }
 }
