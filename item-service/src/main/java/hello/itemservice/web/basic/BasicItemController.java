258bc55a604e2ad61268e8d2b7ca72c4c3d9b8ae
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-02-10 16:33:42 +0900

    [feat/스프링 MVC 웹페이지 만들기] RedirectAttributes : 1) +item.getId()처럼 URL에 변수를 더해서 사용할 시 발생하는 URL 인코딩 문제 해결 2) 상품 등록 시에 redirectAttributes.addAttribute()를 사용하여 status=True를 추가 param으로 붙이도록 설정 -> 타임리프에서 쿼리 파라미터를 편리하게 조회하는 기능을 활용해 상품 상세 뷰에서 param.status가 참이면 저장완료 문구를 출력하도록 추가

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -77,11 +78,21 @@ public class BasicItemController {
     }
 
     // 상품 등록 + PRG
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(Item item){
 
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+
+        Item savedItem = itemRepository.save(item);
+        // redirect와 관련된 속성
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true); // status = True이면 저장되어 넘어왔다고 생각
+        return "redirect:/basic/items/{itemId}";
     }
 
     // 상품 수정
