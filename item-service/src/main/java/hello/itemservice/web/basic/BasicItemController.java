8411e6cbc130ebea155fa22fd751cc297a457159
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-02-10 14:32:13 +0900

    [feat/스프링 MVC 웹페이지 만들기] 상품 등록 폼 : HTML form에서 action에 값이 없으면 현재 URL에 데이터 전송, 상품 등록 폼의 URL(GET)과 실제 상품 등록을 처리하는 URL(POST)를 똑같이 맞추고 HTTP 메서드로 두 기능 분리

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,18 +20,28 @@ public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
-    @GetMapping
+    @GetMapping // 상품 목록
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
     }
 
-    @GetMapping("/{itemId}")
+    @GetMapping("/{itemId}") // 상품 상세
     public String item(@PathVariable("itemId") long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
+
+    @GetMapping("/add") // 상품 등록
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(){
+        return "basic/addForm";
     }
 
     /**
