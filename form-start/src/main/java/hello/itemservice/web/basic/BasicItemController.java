d77023049a29a7cab45796e8659ad12778b3f406
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-02-16 01:16:50 +0900

    [feat/타임리프 스프링 통합과 폼] 입력 폼 처리 : th:object=을 통해 <form>에서 사용할 객체 지정/ th:field=*{itemName}에서 선택 변수식 사용, id,name,value 속성을 자동으로 생성

--- form-start/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ form-start/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -36,7 +36,8 @@ public class BasicItemController {
 
     // 상품 등록
     @GetMapping("/add")
-    public String addForm(){
+    public String addForm(Model model){
+        model.addAttribute("item", new Item());
         return "basic/addForm";
     }
 
