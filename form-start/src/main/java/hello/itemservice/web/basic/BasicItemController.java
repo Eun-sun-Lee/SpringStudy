8fd4b40d179f6876cd5a4227be4959711293a492
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-02-16 02:24:58 +0900

    [feat/스프링 통합과 폼] 체크박스-단일1 : checkBox의 한계를 스프링 MVC가 hidden field를 이용하여 해결 -> [hidden field 미사용시] : 체크박스 체크시 open=on => open=True / 체크박스 미체크시 open이라는 필드 자체가 서버로 전송 X => open=null, [hidden field 사용시] : 체크박스 체크시 open=on&_open=on => open=True / 체크박스 미체크시 : _open=on => open=False로 지정해줌

--- form-start/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ form-start/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -4,6 +4,7 @@ import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
+@Slf4j
 public class BasicItemController {
 
     private final ItemRepository itemRepository;
@@ -56,39 +58,9 @@ public class BasicItemController {
         return "basic/item";
     }
 
-//    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item){
-
-        itemRepository.save(item);
-//        model.addAttribute("item", item); 자동 추가, 생략 가능
-        return "basic/item";
-    }
-
-//    @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item){
-
-        itemRepository.save(item);
-        return "basic/item";
-    }
-
-//    @PostMapping("/add")
-    public String addItemV4(Item item){
-
-        itemRepository.save(item);
-        return "basic/item";
-    }
-
-    // 상품 등록 + PRG
-//    @PostMapping("/add")
-    public String addItemV5(Item item){
-
-        itemRepository.save(item);
-        return "redirect:/basic/items/" + item.getId();
-    }
-
     @PostMapping("/add")
-    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
-
+    public String addItem(Item item, RedirectAttributes redirectAttributes){
+        log.info("item.open={}", item.getOpen());
         Item savedItem = itemRepository.save(item);
         // redirect와 관련된 속성
         redirectAttributes.addAttribute("itemId", savedItem.getId());
