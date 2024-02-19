f90790b71b1137da30319b0f5750405944277b48
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-02-19 15:12:50 +0900

    [feat/검증1 - Validation] Validator 분리1 : 컨트롤러에서 검증 로직이 차지하는 부분이 매우 크므로 ItemValidator라는 별도의 클래스로 분리 -> 1) ItemValidator를 스프링 빈으로 주입 받아서 직접 호출 2) ItemValidator는 스프링이 제공하는 인터페이스인 Validator를 상속 받아 사용 (supports, validate 등의 함수 제공)

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -25,6 +25,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator; // 스프링 빈 주
 
     @GetMapping
     public String items(Model model) {
@@ -163,7 +164,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model){
 
         log.info("objectName={}", bindingResult.getObjectName());
@@ -191,6 +192,24 @@ public class ValidationItemControllerV2 {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) { // 에러가 있으면
+            log.info("errors = {} ", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true); // status = True이면 저장되어 넘어왔다고 생각
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model){
+
+        itemValidator.validate(item, bindingResult);
 
         // 검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) { // 에러가 있으면
